d441dc970244e62826ad5ffa880c4951aa4b75d7
Git-ijhwang <hwangij@gmail.com>
2022-03-16 20:51:29 -0300

    bug fix

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -12,6 +12,8 @@ from .models import Pizza, Size
 class PizzaForm(forms.ModelForm):
     # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.CheckboxSelectMultiple)
     # image = forms.ImageField()
+    # email = forms.EmailField()
+    # url = forms.URLField()
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -19,8 +19,12 @@ def order(request):
             note = 'Thanks for ordering Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
                 filled_form.cleaned_data['topping1'],
                 filled_form.cleaned_data['topping2'],)
-            new_form = PizzaForm()
-            return render(request, 'pizza/order.html', {'created_pizza_pk':created_pizza_pk, 'pizzaform':new_form, 'note':note, 'multiple_form':multiple_form})
+            filled_form = PizzaForm()
+        else:
+            created_pizza_pk = None
+            note = 'Pizza order has filed. Try again.'
+
+        return render(request, 'pizza/order.html', {'created_pizza_pk':created_pizza_pk, 'pizzaform':filled_form, 'note':note, 'multiple_form':multiple_form})
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform':form, 'multiple_form':multiple_form})
@@ -54,7 +58,7 @@ def pizzas(request):
 
 
 def edit_order(request, pk):
-    pizza = Pizza.object.get(pk=pk)
+    pizza = Pizza.objects.get(pk=pk)
     form = PizzaForm(instance=pizza)
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST, instance=pizza)
@@ -62,4 +66,6 @@ def edit_order(request, pk):
         if filled_form.is_valid():
             filled_form.save()
             form = filled_form
+            note = 'Order has been updated!'
+            return render(request, 'pizza/edit_order.html', {'note':note, 'pizzaform':form, 'pizza':pizza})
     return render(request, 'pizza/edit_order.html', {'pizzaform':form, 'pizza':pizza})
