028018ab20106b2c9614c70b6f5b02c5e7c87a29
Git-ijhwang <hwangij@gmail.com>
2022-03-17 08:11:07 -0300

    Registraion form

---
+++ Blog/accounts/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django import forms
+
+
+#Interface at the frontend to get user's input
+
+#HTML's form
+#form to save to DB
+
+class RegistrationForm (forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'firstname',
+                  'lastname', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data #avoid SQL injection attack
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords not matched!')
+        return cd['password2']
